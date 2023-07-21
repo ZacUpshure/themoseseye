d6a951ee562f7930b59f1c467799435b83d8eab1
Zac <zac.upshure@outlook.com>
2023-07-21 19:19:24 +0200

    testing stripe

--- api/stripe.js
+++ api/stripe.js
@@ -5,59 +5,30 @@ const stripe = new Stripe(process.env.REACT_APP_STRIPE_PRIVATE_KEY);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-
-
         const params = {
-            submit_types: 'pay',
+            submit_type: 'pay',
             mode: 'payment',
             payment_method_types: ['card'],
             billing_address_collection: 'auto',
             line_items: req.body.map((item) => {
-              // Make sure to correctly form the image URL
-              const img = item.image[0]?.asset?._ref; // Use optional chaining in case image[0] or asset is missing
-              const newImage = img ? img.replace('image-', `https://cdn.sanity.io/images/${process.env.REACT_APP_SANITY_PROJECT_ID}/production/`).replace('-webp', '.webp') : null;
+              const img = item.image[0].asset._ref;
+              const newImage = img.replace('image-', `https://cdn.sanity.io/images/${process.env.REACT_APP_SANITY_PROJECT_ID}/production/`).replace('-webp', 'webp');
           
               return {
                 price_data: {
                   currency: 'eur',
                   product_data: {
                     name: item.name,
-                    images: newImage ? [newImage] : [], // Ensure you pass the image URL as an array
+                    images: [newImage],
                   },
                   unit_amount: item.price * 100,
                 },
-                quantity: 1, // Fixed quantity of 1 for each product
+                quantity: item.quantity, // Include the quantity for each product
               };
             }),
             success_url: `${req.headers.origin}/?success=true`,
             cancel_url: `${req.headers.origin}/?canceled=true`,
           };
-          
-
-        // const params = {
-        //     submit_type: 'pay',
-        //     mode: 'payment',
-        //     payment_method_types: ['card'],
-        //     billing_address_collection: 'auto',
-        //     line_items: req.body.map((item) => {
-        //       const img = item.image[0].asset._ref;
-        //       const newImage = img.replace('image-', `https://cdn.sanity.io/images/${process.env.REACT_APP_SANITY_PROJECT_ID}/production/`).replace('-webp', 'webp');
-          
-        //       return {
-        //         price_data: {
-        //           currency: 'eur',
-        //           product_data: {
-        //             name: item.name,
-        //             images: [newImage],
-        //           },
-        //           unit_amount: item.price * 100,
-        //         },
-        //         quantity: item.quantity, // Include the quantity for each product
-        //       };
-        //     }),
-        //     success_url: `${req.headers.origin}/?success=true`,
-        //     cancel_url: `${req.headers.origin}/?canceled=true`,
-        //   };
 
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
