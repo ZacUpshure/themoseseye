fc98bbf0f6e530079d6581d4f53b68894410d09b
Zac <zac.upshure@outlook.com>
2023-07-27 19:32:36 +0200

    add missing import

--- src/container/index.js
+++ src/container/index.js
@@ -2,6 +2,7 @@ export { default as About } from './About/About';
 export { default as FooterTwo } from './Footer/FooterTwo';
 export { default as RealFooter } from './Footer/RealFooter';
 export { default as Skills } from './Skills/Skills';
+export { default as Header } from './Header/Header';
 export { default as Testimonial } from './Testimonial/Testimonial';
 export { default as Work } from './Work/Work';
 export { default as Features } from './Features/Features';
